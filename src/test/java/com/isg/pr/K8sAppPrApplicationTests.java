be062c20ddd2cd599a224224a04c65ecc0f46970
vinay-83 <kpit@L-9927>
2020-09-11 14:31:05 +0530

    first commit

---
+++ src/test/java/com/isg/pr/K8sAppPrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isg.pr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K8sAppPrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
