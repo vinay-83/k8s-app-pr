cd41dc5bf0bece480163ee3d028aaf6ad910a273
vinay-83 <vennuvinay7@gmail.com>
2020-09-20 16:24:41 +0530

    getAll added

---
+++ src/main/java/com/isg/pr/UserRepo.java
@@ -0,0 +1,7 @@
+package com.isg.pr;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<UserModel, Integer>{
+
+}
--- src/main/java/com/isg/pr/controller/PrController.java
+++ src/main/java/com/isg/pr/controller/PrController.java
@@ -1,14 +1,38 @@
 package com.isg.pr.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.isg.pr.UserModel;
+import com.isg.pr.UserRepo;
+
 
 @RestController
 public class PrController {
 
+	@Autowired
+	UserRepo userRepo;
+	
 	@GetMapping("/whoAmI")
 	public String whoAmI() {
 		System.out.println("am from pr version 1.0 application");
 		return "am from pr version 1.0 application";
+	}
+	
+	@GetMapping("/addEmp/{id}/{name}")
+	public UserModel saveaddEmp(@PathVariable Integer id, @PathVariable String name) {
+		UserModel um = new UserModel();
+		um.setName(name);
+		um.setUserId(id);
+		return userRepo.save(um);
+	}
+	
+	@GetMapping("/getAllFromMysql")
+	public List<UserModel> getAllFromMysql(){
+		return userRepo.findAll();
 	}
 }
